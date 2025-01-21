a4ae3932b7124283e716f1f40475f1174e4cacd6
Ezekiel <gadiana.jhonezekiel@gmail.com>
2025-01-21 22:37:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./components/navigation";
+import Hero from "./components/hero";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero/>
+    </>
+  );
+}
+
+export default App;
